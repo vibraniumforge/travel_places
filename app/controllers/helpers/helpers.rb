6058faa4665520d26be141c127b0bca0f5005aa7
Greg Morgan <gregm9999@gmail.com>
2018-12-21 17:57:19 +0000

    Done.

--- app/controllers/helpers/helpers.rb
+++ app/controllers/helpers/helpers.rb
@@ -2,6 +2,9 @@ class Helpers
 
   def self.current_user(session)
     @user=User.find_by_id(session[:user_id])
+    if @user.is_a?(User)
+      @user
+    end
   end
 
   def self.is_logged_in?(session)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  include Slugifiable::InstanceMethods
-  extend Slugifiable::ClassMethods
   has_many :tweets
   has_secure_password
 end
