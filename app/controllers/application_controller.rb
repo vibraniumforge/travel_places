20c42160acd8fce99471a69d72de274c488ad3ce
Vibraniumforge <gregm9999@gmail.com>
2019-02-07 15:43:28 -0800

    refactored app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,16 @@ class ApplicationController < Sinatra::Base
     erb :'index'
   end
 
+  def current_user(session)
+    @user=User.find_by_id(session[:user_id])
+    if @user.is_a?(User)
+      @user
+    end
+  end
+
+  def is_logged_in?(session)
+    !!User.find_by_id(session[:user_id])
+  end
+
+
 end
